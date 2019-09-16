f450c8d733509f1aed863f34eec4fa00b82c819e
SnowNooDLe <sdw123123@gmail.com>
2019-09-16 21:15:13 +1200

    Updated version.

--- TkinterDrawer.py
+++ TkinterDrawer.py
@@ -41,7 +41,6 @@ class TkinterDrawer(AbstractDrawer):
         self.file = Writer("TKInterDrawer_Result.txt")
 
     def setup(self):
-
         self.root.geometry("510x645")
         self.choose_size_button = 1
         self.x = 250
@@ -100,24 +99,30 @@ class TkinterDrawer(AbstractDrawer):
 
     def select_pen(self, pen_num):
         self.line_width = self.choose_size_button.get()
+        self.file.writeToFile("Pen size", self.line_width)
 
     def pen_down(self):
         self.pen_state = True
+        self.file.writeToFile("Is pen down ?", self.pen_state)
 
     def pen_up(self):
         self.pen_state = False
+        self.file.writeToFile("Is pen down ?", self.pen_state)
 
     def go_along(self, along):
         self.x = along
+        self.file.writeToFile("New X Position", self.x)
 
     def go_down(self, down):
         self.y = down
+        self.file.writeToFile("New Y Position", self.y)
 
     def draw_line(self, direction, distance):
         if not self.pen_state:
             self.color = "white"
         else:
             self.color = "black"
+        self.file.writeToFile("Pen color", self.color)
 
         if direction == 0:
             new_direction = 0
@@ -128,7 +133,7 @@ class TkinterDrawer(AbstractDrawer):
             new_direction = 270
         if direction == 270:
             new_direction = 90
-
+        
         angle_in_radians = new_direction * math.pi / 180
 
         line_length = distance
@@ -141,26 +146,36 @@ class TkinterDrawer(AbstractDrawer):
         self.c.create_line(self.x, self.y, end_x, end_y, fill=self.color, width=self.line_width)
         self.x = end_x
         self.y = end_y
+        self.file.writeToFile("New X position", self.x)
+        self.file.writeToFile("New Y position", self.y)
 
     def draw_square(self):
+        self.file.writeToFile("We are drawing a square")
         if self.pen_state:
             directions = [0, 90, 180, 270]
             for i in directions:
                 self.draw_line(i, 50)
 
     def draw_circle(self):
+        self.file.writeToFile("We are drawing a circle")
         r = 50
         if self.pen_state:
             self.c.create_oval(self.x - r, self.y - r, self.x + r, self.y + r,width=self.line_width)
 
     def draw_triangle(self):
+        self.file.writeToFile("We are drawing a triangle")
         self.c.create_line(55, 85, 155, 85, 105, 180, 55, 85, width=self.line_width)
 
     def reset(self):
+        self.file.writeToFile("We are restting")
         self.x = 250
         self.y = 250
         self.c.delete("all")
+        self.file.writeToFile("Back to original X coordinate", self.x)
+        self.file.writeToFile("Back to original Y coordinate", self.y)
+
 
     def start(self):
+        self.file.writeToFile("Starting TKinter Drawer. Here we go ! ")
         self.setup()
         self.root.mainloop()
